3016352be622e0105ffede0d59ad7a7c4f904bb4
Alex Sansse <alex2.san@laposte.net>
2020-09-29 18:24:42 +0200

    modif de index et main

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,7 +1,25 @@
-// var message = console.log("Bien le bonjours");
-// console.log(message);
+// console.log('Mon message');
+// console.error('Mon message erreur');
+//document.addEventListener('click', function() {
+//    alert('MESSAGE');
+//});
 
-// console.error("et meerde");
+/*
+const bouton = document.getElementsByClassName("boutonB")
+const btns = document.getElementsByClassName("buttonB")
+btns[0].addEvenlister('click', function() {
+    alert("Message");
+})
+*/
+//
+// autre façon de faire
 
-// alert(" Hello ma couille");
+const monButun = document.getElementById('boutonB');
+if (monButun != null) {
+    monButun.addEventListener('click', function() {
+        const table = document.querySelector('table');
+        table.style.display = "none";
+        alert("test");
+    });
+}
 
