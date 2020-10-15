8276ec382f5da27c562a713055863b165aef385c
Alex Sansse <alex2.san@laposte.net>
2020-10-15 16:23:58 +0200

    modif de main et index

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,3 +1,4 @@
+
 // var message = console.log("Bien le bonjours");
 // console.log(message);
 
@@ -5,3 +6,43 @@
 
 // alert(" Hello ma couille");
 
+
+// AFFICHE / MASQUE LE TABLEAU
+
+const myButton = document.getElementById('boutonB');
+if(myButton != null) {
+    myButton.addEventListener('click', function() {
+       const table = document.querySelector('table');
+       if (table.style.display === 'none') {
+           table.style.display = 'block';
+       } else {
+           table.style.display = 'none';
+       }
+       });
+}
+
+
+// ENLEVE LIGNE DU TABLEAU (Jquery) (tr)
+/*
+$('#boutonB').on('click', function() {
+    $('table').find('tr').last().remove();
+});
+*/
+
+// SUPPRIME UNE LIGNE DU TABLEAU 
+/*
+const btn = document.getElementById("boutonB");
+btn.addEventListener('click', function() {
+    document.querySelectorAll('table tr').deleteRow(-1);
+});
+*/
+
+
+
+// ENVOIS UN MESSAGE SI ON CLICK SUR LE BOUTON
+/*
+const btns = document.getElementsByClassName('btn btn-primary');
+btns[0].addEventListener('click', function(){
+    alert('MESSAGE');
+});
+*/
